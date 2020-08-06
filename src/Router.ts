bcdfb73b4da18afd9d09142f601d58c583fc220b
Alexey Zauzin <lehadnk@gmail.com>
2020-08-07 01:11:57 +0300

    Initial commit

---
+++ src/Router.ts
@@ -0,0 +1,20 @@
+import {DiscordMessage} from "nergal/src/DTO/DiscordMessage";
+import {DiscordControllerResponse} from "nergal/src/DTO/DiscordControllerResponse";
+import IRouter from 'nergal/src/Routing/IRouter';
+
+class DirectMessageController {
+    dispatch(msg: DiscordMessage): Promise<DiscordControllerResponse> {
+        console.log("Handling " + msg.message);
+
+        return new Promise<DiscordControllerResponse>((resolve) => {
+            resolve(new DiscordControllerResponse("Hello!", false));
+        });
+    }
+}
+
+export default class Router implements IRouter {
+    async route(msg: DiscordMessage): Promise<DiscordControllerResponse> {
+        let controller = new DirectMessageController();
+        return await controller.dispatch(msg);
+    }
+}
