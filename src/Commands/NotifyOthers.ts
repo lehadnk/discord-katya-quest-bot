8771d99105b54a973582e7e617eaea6644b68fef
Alexey Zauzin <lehadnk@gmail.com>
2020-10-09 20:12:40 +0300

    Added new script

---
+++ src/Commands/NotifyOthers.ts
@@ -0,0 +1,25 @@
+import {ICommand} from "nergal";
+import {AbstractCommand} from "nergal";
+import User from "../Models/User";
+import GameService from "../Services/Game/GameService";
+import UsersDAO from "../DAO/UsersDAO";
+import NotificationService from "../Services/NotificationService/NotificationService";
+import TakenHintsDAO from "../DAO/TakenHintsDAO";
+import AppServiceContainer from "../AppServiceContainer";
+import DirectMessageService from "../Services/DirectMessages/DirectMessageService";
+
+export default class TestGameComplete extends AbstractCommand implements ICommand {
+    name: string = 'notify-others';
+
+    async run(args: string[]) {
+        let gameService = new GameService();
+        let usersDAO = new UsersDAO(AppServiceContainer.db);
+        let users = await usersDAO.getNonnotified();
+
+        for (const user of users) {
+            let question = await gameService.getCurrentQuestion(user);
+            await AppServiceContainer.messagingService.sendDM(user.discord_user_id, question.text).catch(r => console.log(r));
+            console.log('Sent to '+user.discord_user_id);
+        }
+    }
+}
--- src/Commands/index.ts
+++ src/Commands/index.ts
@@ -1,3 +1,4 @@
 export {default as ListGuilds} from './ListGuilds';
 export {default as ListUsers} from './ListUsers';
 export {default as NotifyStart} from './NotifyStart';
+export {default as NotifyOthers} from './NotifyOthers';
